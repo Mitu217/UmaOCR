f59d26a616c93c39278e8855aa8670910aad2c7c
Mitu217 <mtksk.26884um@gmail.com>
2021-08-28 18:09:15 +0900

    fix ocr library

--- app/library/ocr.py
+++ app/library/ocr.py
@@ -27,7 +27,7 @@ def get_text_with_single_text_line_and_eng_from_image(image: Image):
 
 
 def get_line_box_with_single_text_line_and_jpn_from_image(image: Image):
-    builder = pyocr.builders.LineBoxBuilder(tesseract_layout=6)
+    builder = pyocr.builders.LineBoxBuilder(tesseract_layout=7)
     text = tool.image_to_string(image, lang="jpn", builder=builder)
 
     return text
