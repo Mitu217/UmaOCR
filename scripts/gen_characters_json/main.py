61dfed3217c14a2c8d4aebec075e9de54787708f
Mitu217 <mtksk.26884um@gmail.com>
2021-09-24 19:21:09 +0900

    update character.json

--- scripts/gen_characters_json/main.py
+++ scripts/gen_characters_json/main.py
@@ -1,6 +1,7 @@
 import json
 import math
 import os
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,28 +9,45 @@ from PIL import Image, ImageDraw, ImageFont
 
 # 【ウマ娘】全キャラの育成論と難易度一覧 - ゲームウィズ(GameWith)
 url_game_with_characters = 'https://gamewith.jp/uma-musume/article/show/253241'
+# テーブル/育成ウマ娘詳細一覧 - ウマ娘wiki
+url_uma_wiki_characters = 'https://umamusume.wikiru.jp/index.php?cmd=read&page=%A5%C6%A1%BC%A5%D6%A5%EB%2F%B0%E9%C0%AE%A5%A6%A5%DE%CC%BC%BE%DC%BA%D9%B0%EC%CD%F7'
 
 response_game_with_characters = requests.get(url_game_with_characters)
 soup_game_with_characters = BeautifulSoup(response_game_with_characters.text, "html.parser")
 
+response_uma_wiki_characters = requests.get(url_uma_wiki_characters)
+soup_uma_wiki_characters = BeautifulSoup(response_uma_wiki_characters.text, "html.parser")
+
 
 def get_characters():
-    soup = soup_game_with_characters
+    soup = soup_uma_wiki_characters
     data = []
 
-    for table in soup.select('table'):
-        if table.parent.get('class', False) is False:
-            continue
-        if table.parent['class'][0] == 'umamusume-ikusei-ichiran':
-            for tr in table.find_all('tr')[1:]:  # skip header
-                tds = tr.find_all('td')
-                if len(tds) < 1:
-                    continue
-                name = normalize_name(tds[0].find('a').text)
-                if len(name) == 0:
-                    continue
+    for table in soup.select('table.style_table'):
+        for tr in table.find_all('tr')[1:]:  # skip header
+            tds = tr.find_all('td')
+            if len(tds) < 1:
+                continue
+            text = normalize_name(tds[0].find('a').text)
+            if len(text) == 0:
+                continue
+
+            nicknames = re.findall("(?<=［).+?(?=］)", text)
+            if len(nicknames) == 0:
+                continue
+            nickname = normalize_nickname(nicknames[0])
+            name = normalize_name(re.sub("［.+?］", "", text))
+
+            found = False
+            for character in data:
+                if character['name'] == name:
+                    found = True
+                    character['nickname'].append(nickname)
+                    break
+            if not found:
                 data.append({
                     'name': name,
+                    'nickname': [nickname],
                 })
 
     return sorted(data, key=lambda x: x['name'])
@@ -43,6 +61,16 @@ def normalize_name(name):
         return ''
 
     return name
+
+def normalize_nickname(nickname):
+    if 'エル☆Numero 1' == nickname:
+        return 'エル☆Número 1'
+    if 'オーセンティック／1928' == nickname:
+        return 'オーセンティック/1928'
+    if 'ブルー／レイジング' == nickname:
+        return 'ブルー/レイジング'
+
+    return nickname
 
 if __name__ == "__main__":
     characters = get_characters()
