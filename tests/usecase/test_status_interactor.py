7fb6a12b951c5319e2499d596e387cebfc29d026
Mitu217 <mtksk.26884um@gmail.com>
2021-09-25 21:58:18 +0900

    add test_get_parameters_from_image

--- tests/usecase/test_status_interactor.py
+++ tests/usecase/test_status_interactor.py
@@ -7,12 +7,50 @@ from unittest import TestCase
 from PIL import Image
 
 import resources
-from app.domain.parameters import SupportParameters
+from app.domain.parameters import Parameters, SupportParameters
 from app.driver.file_driver import LocalFileDriverImpl
 from app.usecase.status_interactor import StatusInteractor
+from app.usecase.image import ImageInteractor
 
 
 class TestSkillInteractor(TestCase):
+    def test_get_parameters_from_image(self) -> None:
+        for image_name, result_name in (
+                ('image49.png', 'image49.csv'),
+        ):
+            with self.subTest(image_name=image_name):
+                status_interactor = StatusInteractor(
+                    LocalFileDriverImpl(''),
+                    logging.getLogger(__name__),
+                )
+                image_interactor = ImageInteractor(
+                    LocalFileDriverImpl(''),
+                    logging.getLogger(__name__),
+                )
+                with Image.open(
+                        os.path.join(resources.__path__[0], 'tests',
+                                     'get_parameters_from_image', image_name)) as image:
+                    character_detail_image = asyncio.run(
+                        image_interactor.create_character_detail_image(image))  # TODO: テスト独自のhelper関数へ変更する
+                    got = asyncio.run(status_interactor.get_parameters_from_image(character_detail_image))
+                with open(
+                        os.path.join(resources.__path__[0], 'tests',
+                                     'get_parameters_from_image', result_name)) as result:
+                    wants = []
+                    reader = csv.reader(result)
+                    for row in reader:
+                        wants.append(
+                            Parameters(
+                                row[0],
+                                row[1],
+                                row[2],
+                                row[3],
+                                row[4],
+                            )
+                        )
+                    want = wants[0]
+                self.assertEqual(got, want)
+
     def test_get_support_parameters_from_image(self) -> None:
         for image_name, result_name in (
             ('normal1.jpg', 'normal1.csv'),
